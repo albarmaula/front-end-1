54ba7d861f90ea9a880fbb82473c3c205d81abb1
Widaad Albar M <albarmaula@gmail.com>
2023-10-16 13:13:00 +0700

    belajar MUI#2 :: Fix SignIn & SignUp

--- app/signUp/page.js
+++ app/signUp/page.js
@@ -1,6 +1,11 @@
 "use client";
 import * as React from "react";
 import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Grid from "@mui/material/Grid";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
@@ -11,106 +16,202 @@ import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
 import Link from "next/link";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+import Paper from "@mui/material/Paper";
 
-export default function LoginPage() {
-  const [showPassword, setShowPassword] = React.useState(false);
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="#333" align="center" {...props} className="mt-3">
+      {"Copyright © "}
+      <Link color="inherit" href="#">
+        Widaad's App. RPL. 1201200022.
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
+export default function SignUpPage() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
   const linkStyle = {
-    color: 'blue', // Change the color to your preferred color
-    textDecoration: 'none',
-    '&:hover': {
-      color: 'red', // Change the hover color to your preferred color
+    color: "blue", // Change the color to your preferred color
+    textDecoration: "none",
+    "&:hover": {
+      color: "red", // Change the hover color to your preferred color
     },
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-800">
-      <Box
-        sx={{ display: "flex", flexWrap: "wrap" }}
-        className="bg-white shadow-md p-8 rounded-lg w-80 bg-gray-800 justify-center"
+    <div className="flex min-h-screen bg-gray-800">
+      <Grid
+        container
+        component="main"
+        className="mr-12 ml-12 mt-10 mb-10 border rounded-lg"
       >
-        <h1 className="font-semibold text-center mb-4 text-gray-800">Sign Up</h1>
-        <TextField
-          sx={{ m: 1, width: "25ch" }}
-          variant="outlined"
-          label="Username"
-          className="w-full border rounded px-3 py-2 text-gray-800 border-gray-800"
+        <CssBaseline />
+        
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          className="bg-white shadow-md pl-10 pr-10 pt-1 pb-1"
+          style={{
+            borderTopLeftRadius: "0.5rem",
+            borderBottomLeftRadius: "0.5rem",
+          }}
         >
-          <OutlinedInput
-            id="outlined-adornment-username"
-            endAdornment={<InputAdornment>Username</InputAdornment>}
-            aria-describedby="outlined-weight-helper-text"
-            inputProps={{
-              Radiance: "weight",
-            }}
-          />
-        </TextField>
-        <TextField
-          sx={{ m: 1, width: "25ch" }}
-          variant="outlined"
-          label="Email"
-          className="w-full border rounded px-3 py-2 text-gray-800 border-gray-800"
-        >
-          <OutlinedInput
-            id="outlined-adornment-email"
-            endAdornment={<InputAdornment>Email</InputAdornment>}
-            aria-describedby="outlined-weight-helper-text"
-            inputProps={{
-              Radiance: "weight",
-            }}
-          />
-        </TextField>
-        <FormControl
-          sx={{ m: 1, width: "25ch" }}
-          variant="outlined"
-          className="w-full border rounded px-3 py-2 text-gray-800 border-gray-800"
-        >
-          <InputLabel htmlFor="outlined-adornment-password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          className="bg-gray-600 hover:bg-gray-500"
-          size="large"
-          href="../pages/home"
-        >
-          Sign Up
-        </Button>
-        <p className="mt-3">
-          Already have account?{" "}
-          <Link
-            href="../"
-            style={linkStyle}
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            className="p-2"
           >
-            Login
-          </Link>
-          .
-        </p>
-      </Box>
+            <Avatar sx={{ m: 1, bgcolor: "#333" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+          </Box>
+
+          <h1 className="text-center">Sign Up</h1>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  autoComplete="new-password"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I proceeded to sign up for the service, carefully reading and ultimately agreeing to the terms and conditions"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              className="bg-gray-800 hover:bg-gray-600"
+              href="/pages/home"
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                Already have an account?
+                <Link href="../" variant="body2" style={linkStyle}>
+                  Sign In
+                </Link>
+              </Grid>
+            </Grid>
+            <Copyright/>
+          </Box>
+          
+        </Grid>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+          }}
+          style={{
+            borderTopRighttRadius: "0.5rem",
+            borderBottomRightRadius: "0.5rem",
+          }}
+        />
+      </Grid>
     </div>
   );
 }
